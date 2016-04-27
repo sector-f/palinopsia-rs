3afd9ccac725ef5082ccfc96d7673abe4badd5c1
Adam <adamsimeth@gmail.com>
2016-04-27 16:41:53 -0400

    Partially unbroke it?

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 use image::png;
+use image::Pixel;
 
 fn usage() -> ! {
     let programname = args_os().nth(0).unwrap();
-    let message = format!("Usage: {} HEIGHT WIDTH MEGABYTES\n", programname.to_string_lossy());
+    let message = format!("Usage: {} [-v] HEIGHT WIDTH MEGABYTES\n", programname.to_string_lossy());
     let _ = stderr().write_all(message.as_bytes());
     exit(1);
 }
@@ -27,17 +28,34 @@ fn arg_to_int(arg: u32) -> u32 {
 }
 
 fn main() {
-    if args_os().count() != 4 {
+    if args_os().count() != 4 && args_os().count() != 5 {
         usage();
     }
 
+    let verbose = if args_os().nth(1).unwrap().to_string_lossy() == "-v" ||
+        args_os().nth(1).unwrap().to_string_lossy() == "--verbose" {
+        true
+    } else {
+        false
+    };
+
     let mut current: isize = 0;
 
-    let width = arg_to_int(1);
-    let height = arg_to_int(2);
-    let num_buffers = (arg_to_int(3) * 1024 * 1024) / (width * height * 4);
+    let (width, height) = if ! verbose {
+        (arg_to_int(1), arg_to_int(2))
+    } else {
+        (arg_to_int(2), arg_to_int(3))
+    };
 
-    println!("{}", num_buffers);
+    let num_buffers = if ! verbose {
+        (arg_to_int(3) * 1024 * 1024) / (width * height * 4)
+    } else {
+        (arg_to_int(4) * 1024 * 1024) / (width * height * 4)
+    };
+
+    if verbose {
+        println!("{}", num_buffers);
+    }
 
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
@@ -48,10 +66,12 @@ fn main() {
     let mut txt: Vec<render::Texture> = Vec::new();
 
     for i in 0..num_buffers {
-        println!("Now initializing buffer number {} of {}", i, num_buffers);
+        if verbose {
+            println!("Now initializing buffer number {} of {}", i, num_buffers);
+        }
 
         txt.push(renderer.create_texture(
-            pixels::PixelFormatEnum::ARGB8888,
+            pixels::PixelFormatEnum::RGBA8888,
             render::TextureAccess::Streaming,
             width,
             height).unwrap());
@@ -84,12 +104,26 @@ fn main() {
                         Some(Keycode::Space) => {
                             let path = format!("frame_{}.png", current);
                             let file = fs::File::create(path).unwrap();
+
                             let (w, h) = renderer.output_size().unwrap();
-                            println!("Saving frame {}", current);
-                            let format = pixels::PixelFormatEnum::ARGB8888;
+
+                            let format = pixels::PixelFormatEnum::RGBA8888;
+
                             let pixels = renderer.read_pixels(None, format).unwrap();
+
+                            let mut image: image::RgbaImage = image::ImageBuffer::from_raw(w, h, pixels).unwrap();
+                            for pixel in image.pixels_mut() {
+                                let mut new_pixel: image::Rgba<u8> = image::Rgba::from_channels(0,0,0,0xff);
+                                new_pixel.blend(pixel);
+                                *pixel = new_pixel;
+                                // pixel[3] = 0xff;
+                            }
+
                             let encoder = png::PNGEncoder::new(file);
-                            let _ = encoder.encode(&pixels, w, h, image::ColorType::RGBA(8));
+
+                            let _ = encoder.encode(&image, w, h, image::ColorType::RGBA(8));
+
+                            println!("Saved frame {}", current);
                         },
                         _ => {}
                     }
@@ -97,7 +131,9 @@ fn main() {
                     if current < 0 {
                         current = current as isize + num_buffers as isize;
                     }
-                    println!("Now displaying buffer {} of {}", current, num_buffers);
+                    if verbose {
+                        println!("Now displaying buffer {} of {}", current, num_buffers);
+                    }
                 },
                 Event::Quit {..} => {
                     break 'running
